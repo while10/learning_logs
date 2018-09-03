e17dccf407935588a0fd89d959e924e034d67f38
while10 <while10@qq.com>
2018-09-03 10:46:16 +0800

    Added get_object_404

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, Http404
 from django.core.urlresolvers import reverse
 from django.contrib.auth.decorators import login_required
+
 
 
 from .models import Topic, Entry
@@ -87,7 +88,7 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
     """显示单个主题及其所有条目"""
-    topic = Topic.objects.get(id=topic_id)
+    topic = get_object_or_404(Topic, id=topic_id)
     #确认请求的主题属于当前用户
     if topic.owner != request.user:
         raise Http404
